0753e9a0a74272490035bdeaea2938f006c3be5d
totul01744 <39863463+totul01744@users.noreply.github.com>
2026-04-01 16:09:27 +0600

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -7,15 +7,14 @@
    ৪. নিচের PASTE_YOUR_... গুলো replace করুন
 */
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD8nRvZXgaJnjHS1mGbKvq1ekBOdEIIjd4",
-  authDomain: "make2-a4656.firebaseapp.com",
-  databaseURL: "https://make2-a4656-default-rtdb.firebaseio.com",
-  projectId: "make2-a4656",
-  storageBucket: "make2-a4656.firebasestorage.app",
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyD8nRvZXgaJnjHS1mGbKvq1ekBOdEIIjd4",
+  authDomain:        "make2-a4656.firebaseapp.com",
+  databaseURL:       "https://make2-a4656-default-rtdb.firebaseio.com",
+  projectId:         "make2-a4656",
+  storageBucket:     "make2-a4656.firebasestorage.app",
   messagingSenderId: "763548170013",
-  appId: "1:763548170013:web:ae76ebb6f8f5e5013bd3f4",
-  measurementId: "G-RGWYMVP2B5"
+  appId:             "763548170013:web:ae76ebb6f8f5e5013bd3f4"
 };
 
 /* Admin password — এখানে পরিবর্তন করুন */
